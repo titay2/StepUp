e53217653ced1dccc9eed6f6ef287cf68759992c
titay2 <tehetenamas@gmail.com>
2016-09-28 13:04:13 +0300

    fading logo

--- app/src/main/java/com/example/tehetenamasresha/layout/MainActivity.java
+++ app/src/main/java/com/example/tehetenamasresha/layout/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageView sim = (ImageView) findViewById(R.id.sim);
+
         sim.animate().alpha(0f).setDuration(5000);
 
     }
