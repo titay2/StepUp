fa04cabab7a06594fc000be9605350bc567b41ba
Hai Phan <hai.phan@neighbourfood.fi>
2016-10-13 00:46:35 +0300

    modified User singleton

--- app/src/main/java/com/example/bang/stepup3/User.java
+++ app/src/main/java/com/example/bang/stepup3/User.java
@@ -10,6 +10,7 @@ public class User {
     private static final User INSTANCE = new User();
 
     private User() {
+        foodList = new ArrayList<FoodItem>();
         FoodItem hamburger = new FoodItem("hamburger2", "Hamburger", 1000.0, 1000, 1000.0, 1000);
         FoodItem hotdog = new FoodItem("hotdog", "Hot Dog", 1200.0, 1200, 1200.0, 1200);
         FoodItem burrito = new FoodItem("burrito", "Chicken Burritos", 100.0, 100, 100.0, 1000);
